7dadc988c2d093b4d3353335edfe535fea685511
masterjr <juniormaster@mail.ru>
2021-03-10 00:22:32 +0300

    Lesson_1.1 added

--- Lesson_1/Lesson_1.1.py
+++ Lesson_1/Lesson_1.1.py
@@ -1,1 +1,20 @@
 import requests
+import json
+
+
+url = 'https://api.github.com'
+user='masterjr86'
+
+r = requests.get(f'{url}/users/{user}/repos')
+
+
+for i in r.json():
+    print(i['name'])
+
+
+with open('data.json', 'w') as f:
+    json.dump(r.json(), f)
+
+
+
+
